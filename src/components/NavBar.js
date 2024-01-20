b4fef1cdd5f5f7fec386e17ded48ab32411c2c0c
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-20 17:05:40 +0000

    add toggle functionality to burger menu to collapse when the user clicks outside the manu or a menu item

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css';
@@ -14,6 +14,21 @@ import axios from 'axios';
 function NavBar() {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const [expanded, setExpanded] = useState(false);
+    const ref = useRef(null);
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target))  {
+                setExpanded(false);
+            }
+        };
+
+        document.addEventListener('mouseup', handleClickOutside);
+        return () => {
+            document.removeEventListener('mouseup', handleClickOutside);
+        }
+    }, [ref]);
 
     const handleSignOut = async () => {
         try {
@@ -82,7 +97,7 @@ function NavBar() {
 
     return (
         <div>
-            <Navbar className={styles.NavBar} expand="md" fixed="top">
+            <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
                 <Container>
                     <NavLink to="/">
                         <Navbar.Brand>
@@ -90,7 +105,11 @@ function NavBar() {
                         </Navbar.Brand>
                     </NavLink>
                     {currentUser && addPostIcon}
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle 
+                        ref = {ref}
+                        onClick={() => setExpanded(!expanded)} 
+                        aria-controls="basic-navbar-nav"
+                    />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto text-left">
                             <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
