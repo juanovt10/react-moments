2f5edd0d174bc3789325734b7a810442a0ad57b4
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-21 18:44:11 +0000

    Create code boilerplate for comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,10 +8,16 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/AxiosDefaults";
 import Post from "./Post"
+import CommentCreateForm from "../comments/CommentsCreateForm";
+import { useCurrentUser } from "../../context/CurrentUserContext";
 
 function PostPage() {
     const {id} = useParams();
     const [posts, setPosts] = useState({results: []});
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
 
     useEffect(() => {
         const handleMount = async () => {
@@ -36,7 +42,18 @@ function PostPage() {
                 <p>Popular profiles for mobile</p>
                 <Post {...posts.results[0]} setPosts={setPosts} postPage />
                 <Container className={appStyles.Content}>
-                    Comments
+                    {currentUser ? (
+                        <CommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            post={id}
+                            setPosts={setPosts}
+                            setComments={setComments}
+                        />
+                        ) : comments.results.length ? (
+                            "Comments"
+                        ) : null
+                    }
                 </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
