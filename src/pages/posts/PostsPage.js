ec241430706bbc96dfea9dbd73bc118884b297d3
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-21 13:44:20 +0000

    Fix bug by passing results: data object into setPosts in useEffect

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -24,7 +24,7 @@ function PostsPage({ message, filter=""}) {
         const fetchPosts = async () => {
             try {
                 const {data} = await axiosReq.get(`/posts/?${filter}`);
-                setPosts(data);
+                setPosts({ results: data});
                 setHasLoaded(true);
             } catch(err) {
                 console.log(err);
@@ -38,29 +38,29 @@ function PostsPage({ message, filter=""}) {
   
     return (
         <Row className="h-100">
-        <Col className="py-2 p-0 p-lg-2" lg={8}>
-            <p>Popular profiles mobile</p>
-            {hasLoaded ? (
-                <>
-                    {posts.results?.length ? (
-                        posts.results.map((post) => (
-                            <Post key={post.id} {...post} setPosts={setPosts} />
-                        )) 
-                    ) : (
-                        <Container className={appStyles.Container}>
-                            <Asset src={NoResults} message={message} />
-                        </Container>
-                    )}
-                </>
-            ) : (
-                <Container className={appStyles.Content}>
-                    <Asset spinner />
-                </Container>
-            )}
-        </Col>
-        <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-            <p>Popular profiles for desktop</p>
-        </Col>
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <p>Popular profiles mobile</p>
+                {hasLoaded ? (
+                    <>
+                        {posts.results?.length ? (
+                            posts.results.map((post) => (
+                                <Post key={post.id} {...post} setPosts={setPosts} />    
+                            )) 
+                        ) : (
+                            <Container className={appStyles.Container}>
+                                <Asset src={NoResults} message={message} />
+                            </Container>
+                        )}
+                    </>
+                ) : (
+                    <Container className={appStyles.Content}>
+                        <Asset spinner />
+                    </Container>
+                )}
+            </Col>
+            <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+                <p>Popular profiles for desktop</p>
+            </Col>
         </Row>
     );
 }
