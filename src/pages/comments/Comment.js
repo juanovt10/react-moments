7a9112ac166ff7e667e80913994526cfcc43abb6
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-23 18:27:13 +0000

    Add delete comment functionality

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,6 +3,9 @@ import styles from "../../styles/Comment.module.css";
 import { Media } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import Avatar from '../../components/Avatar';
+import { useCurrentUser } from '../../context/CurrentUserContext';
+import MoreDropdown from '../../components/MoreDropdown';
+import { axiosRes } from '../../api/AxiosDefaults';
 
 export const Comment = (props) => {
     const {
@@ -11,7 +14,32 @@ export const Comment = (props) => {
         owner,
         updated_at,
         content,
-    } = props
+        id,
+        setPost,
+        setComments,
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}/`)
+            setPost(prevPost => ({
+                results: [{
+                    ...prevPost.results[0],
+                    comments_count: prevPost.results[0].comments_count - 1
+                }]
+            }))
+
+            setComments(prevComments => ({
+                ...prevComments,
+                results: prevComments.results.filter(comment => comment.id !== id),
+            }))
+        } catch(err) {
+
+        }
+    }
 
     return (
         <div>
@@ -25,6 +53,9 @@ export const Comment = (props) => {
                     <span className={styles.Date}>{updated_at}</span>
                     <p>{content}</p>
                 </Media.Body>
+                {is_owner && (
+                    <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete}/>
+                )}
             </Media>
         </div>
     )
